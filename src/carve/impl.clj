fe9ae5bafdd93d735474c17c829031e387938ba9
Erik Assum <erik@assum.net>
2020-01-02 23:20:19 +0100

    work in progress

--- src/carve/impl.clj
+++ src/carve/impl.clj
@@ -3,6 +3,8 @@
   (:require
    [clojure.edn :as edn]
    [clojure.java.io :as io]
+   [clojure.set :as set]
+   [clj-kondo.core :as clj-kondo]
    [rewrite-cljc.node :as node]
    [rewrite-cljc.zip :as z]))
 
@@ -20,6 +22,22 @@
     (when-not (.exists ignore-file) (.createNewFile ignore-file))
     (spit carve-ignore-file s :append true)))
 
+
+(defn ns+name [ep] [(symbol (namespace ep)) (symbol (name ep))])
+
+(defn unused-vars-data [removed ignore-vars ignore-from-config
+                        {:keys [var-definitions var-usages] :as analysis}]
+  (let [ignore-from-config (map ns+name ignore-from-config)
+        ignore (map ns+name ignore-vars)
+        definitions-by-ns+name (index-by (juxt :ns :name) var-definitions)
+        defined-vars (set (map (juxt :ns :name) var-definitions))
+        used-vars (->> var-usages (remove :recursive) (map (juxt :to :name)) set)]
+    (->> [ignore-from-config ignore removed]
+         (reduce into used-vars)
+         (set/difference defined-vars)
+         (map definitions-by-ns+name)
+         (group-by :filename))))
+
 (defn interactive [{:keys [:carve-ignore-file]} sym]
   (println (format "Type Y to remove or i to add %s to %s" sym carve-ignore-file))
   (let [input (read-line)]
@@ -27,49 +45,10 @@
       (add-to-carve-ignore-file carve-ignore-file (str sym "\n")))
     input))
 
-(defmulti preamble :format)
-
-(defmethod preamble :default [& _] "")
-
-(defmethod preamble :edn [& _] "[")
-
-(defmethod preamble :interactive [{:keys [file]}]
-  (str "Carving " file "\n"))
-
-(defmulti report :format)
-
-(defmethod report :interactive [{:keys [node]}]
-  (str "Found unused var:\n"
-       "------------------\n"
-       (node/string node)
-       "\n"
-       "------------------\n"))
-
-(defmulti postamble :format)
-
-(defmethod postamble :default [& _] "")
-
-(defmethod postamble :edn [& _] "]")
-
-(defmethod report :text [{:keys [node loc file var]}]
-  (let [[row col] loc]
-    (str file ":" row ":" col " " var "\n")))
-
-(defmethod report :edn [{:keys [node loc file var]}]
-  (let [[row col] loc]
-    {:row row
-     :col col
-     :file file
-     :var var}))
-
-(defn remove-locs [file zloc locs locs->syms {:keys [:interactive?
-                                                     :dry-run?
-                                                     :format]
-                                              :or {interactive? true
-                                                   format :interactive}
-                                              :as opts}]
-  (when (seq locs)
-    (print (preamble {:file file :format format})))
+(defn remove-locs [zloc locs locs->syms {:keys [:interactive?
+                                                :dry-run?]
+                                         :or {interactive? true}
+                                         :as opts}]
   (loop [zloc zloc
          locs (seq locs)
          made-changes? false]
@@ -79,7 +58,10 @@
             m (meta node)]
         (if (and (= row (:row m))
                  (= col (:col m)))
-          (do (println (str file ":" row ":" col " " (get locs->syms loc)))
+          (do (println "Found unused var:")
+              (println "------------------")
+              (println (node/string node))
+              (println "------------------")
               (let [remove? (cond dry-run? false
                                   interactive?
                                   (= "Y" (interactive opts (get locs->syms loc)))
@@ -97,20 +79,26 @@
       (= 'clojure.core/defprotocol defined-by)
       (= 'clojure.core/definterface defined-by)))
 
+(defn locs->syms [vars api-namespaces]
+  (into {}
+        (keep (fn [{:keys [:row :col :ns :name :private :test] :as var-info}]
+                (when (and (not test)
+                           (not (ignore? var-info))
+                           (or (not (contains? api-namespaces ns))
+                               private))
+                  [[row col] (symbol (str ns) (str name))])) vars)))
+
 (defn carve [file vs {:keys [:out-dir :api-namespaces]
                       :as opts}]
   (let [zloc (z/of-file file)
-        locs->syms (into {}
-                         (keep (fn [{:keys [:row :col :ns :name :private :test] :as var-info}]
-                                 (when (and (not test)
-                                            (not (ignore? var-info))
-                                            (or (not (contains? api-namespaces ns))
-                                                private))
-                                   [[row col] (symbol (str ns) (str name))])) vs))
+        locs->syms (locs->syms vs api-namespaces)
         locs (keys locs->syms)
         locs (sort locs)
+        _ (when (seq locs)
+            (println "Carving" file)
+            (println))
         {:keys [:made-changes? :zloc]}
-        (remove-locs file zloc locs locs->syms opts)]
+        (remove-locs zloc locs locs->syms opts)]
     (when made-changes?
       (let [out-file (io/file out-dir file)
             out-file (.getCanonicalFile out-file)]
@@ -118,3 +106,56 @@
         (println "Writing result to" (.getPath out-file))
         (with-open [w (io/writer out-file)]
           (z/print-root zloc w))))))
+
+(defn carve! [removed {:keys [ignore-vars paths carve-ignore-file aggressive?] :as opts}]
+  (let [unused-vars-data (unused-vars-data removed ignore-vars
+                                           (read-carve-ignore-file carve-ignore-file)
+                                           (:analysis (clj-kondo/run! {:lint paths :config {:output {:analysis true}}})))]
+    (when (seq unused-vars-data)
+      (doseq [[file vars] unused-vars-data]
+        (carve file vars opts))
+      (when aggressive?
+        (recur (into removed unused-vars-data) opts)))))
+
+(defn remove-locs [zloc locs locs->syms {:keys [:interactive?
+                                                :dry-run?]
+                                         :or {interactive? true}
+                                         :as opts}]
+  (loop [zloc zloc
+         locs (seq locs)
+         made-changes? false]
+    (if locs
+      (let [[row col :as loc] (first locs)
+            node (z/node zloc)
+            m (meta node)]
+        (if (and (= row (:row m))
+                 (= col (:col m)))
+          (do (println "Found unused var:")
+              (println "------------------")
+              (println (node/string node))
+              (println "------------------")
+              (let [remove? (cond dry-run? false
+                                  interactive?
+                                  (= "Y" (interactive opts (get locs->syms loc)))
+                                  :else true)
+                    zloc (if remove? (z/remove zloc) (z/next zloc))]
+                (recur zloc (next locs) (or remove? made-changes?))))
+          (recur (z/next zloc) locs made-changes?)))
+      {:zloc zloc
+       :made-changes? made-changes?})))
+
+(defn make-report [[file vars] {:keys [api-namespaces] :as opts}]
+  (->> api-namespaces
+       (locs->syms vars)
+       (map (fn [[loc symbol]]
+              {:file file :row (first loc) :col (second loc) :symbol symbol}))
+       (sort-by :row)))
+
+(defn report! [{:keys [ignore-vars paths carve-ignore-file aggressive?] :as opts}]
+  (let [unused-vars-data (unused-vars-data #{} ignore-vars
+                                           (read-carve-ignore-file carve-ignore-file)
+                                           (:analysis (clj-kondo/run! {:lint paths :config {:output {:analysis true}}})))]
+    (when (seq unused-vars-data)
+      (let [file-reports (mapcat #(make-report % opts) unused-vars-data)]
+        (doseq [{:keys [file symbol row col]} file-reports]
+          (println (str file ":" row ":" col " " symbol)))))))
--- src/carve/main.clj
+++ src/carve/main.clj
@@ -1,26 +1,10 @@
 (ns carve.main
   (:require
    [carve.impl :as impl]
-   [clj-kondo.core :as clj-kondo]
    [clojure.edn :as edn]
    [clojure.set :as set]))
 
 (set! *warn-on-reflection* true)
-
-(defn ns+name [ep] [(symbol (namespace ep)) (symbol (name ep))])
-
-(defn unused-vars-data [removed ignore-vars ignore-from-config paths analysis
-                        {:keys [var-definitions var-usages] :as analysis}]
-  (let [ignore-from-config (map ns+name ignore-from-config)
-        ignore (map ns+name ignore-vars)
-        definitions-by-ns+name (impl/index-by (juxt :ns :name) var-definitions)
-        defined-vars (set (map (juxt :ns :name) var-definitions))
-        used-vars (->> var-usages (remove :recursive) (map (juxt :to :name)) set)]
-    (->> [ignore-from-config ignore removed]
-         (reduce into used-vars)
-         (set/difference defined-vars)
-         (map definitions-by-ns+name)
-         (group-by :filename))))
 
 (defn parse-opts [opts]
   (-> opts
@@ -33,17 +17,8 @@
 (defn -main [& [flag opts & _args]]
   (when-not (= "--opts" flag)
     (throw (ex-info (str "Unrecognized option: " flag) {:flag flag})))
-  (let [{:keys [:ignore-vars
-                :paths
-                :carve-ignore-file
-                :aggressive?]} (parse-opts (edn/read-string opts))]
+  (let [opts (parse-opts (edn/read-string opts))]
     (when (empty? (:paths opts)) (throw (ex-info ":paths must not be empty" opts)))
-    (loop [removed #{}]
-      (let [unused-vars-data (foo removed ignore-vars
-                                  (impl/read-carve-ignore-file carve-ignore-file)
-                                  (clj-kondo/run! {:lint paths :config {:output {:analysis true}}}))]
-        (when (seq unused-vars-data)
-          (doseq [[file vs] unused-vars-data]
-            (impl/carve file vs opts))
-          (when aggressive?
-            (recur (into removed unused-vars))))))))
+    (if-not (:dry-run? opts)
+      (impl/carve! #{} opts)
+      (impl/report! opts))))
